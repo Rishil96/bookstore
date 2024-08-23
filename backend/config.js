6a823f16526996b6411fbffb8963c88717b48557
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-23 18:02:58 +0530

    Add : Environment variable usage for config.js file

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,8 @@
-export const PORT = 5555;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const PORT = process.env.PORT;
+export const dbUserName = process.env.DATABASE_USERNAME;
+export const dbPassword = process.env.DATABASE_PASSWORD;
+export const mongoDBURL = `mongodb+srv://${dbUserName}:${dbPassword}@bookstore.lqz8n.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore`;
