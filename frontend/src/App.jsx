3d8d7db041a1ee96494e78d6d468bcb4b2832289
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-08-24 15:51:54 +0530

    Add : Routers and Initial Component files and structure that will act as the frontend to perform the operations related to the book store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'; 
+import CreateBook from './pages/CreateBooks'; 
+import EditBook from './pages/EditBook'; 
+import ShowBook from './pages/ShowBook'; 
+import DeleteBook from './pages/DeleteBook'; 
+
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    /*
+      Routes is a container that holds all routes
+      Route is a single path/URL linked with a component that will be displayed when we go to that path via some event (e.g. clicking a button, etc)
+    */
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
